e67820cba8c5ce0259efd2fa865f73975d84274d
Tudor Ciupe <ciupe.tudor@gmail.com>
2020-11-20 06:12:51 +0000

    created rt.tf

--- sec_group.tf
+++ sec_group.tf
@@ -1,11 +1,19 @@
-resource "aws_security_group" "vpc_task" {
-  name        = "vpc_task"
+resource "aws_security_group" "VPC_PROJECT" {
+  name        = "VPC_PROJECT"
   description = "Allow TLS inbound traffic"
 
   ingress {
     description = "Allow ssh"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow ssh"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
